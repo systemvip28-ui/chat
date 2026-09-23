d021d1176be74042f1eccafab1bf16135e369f83
sanz <systemvip28@gmail.com>
2026-09-23 09:22:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -659,6 +659,7 @@ io.on('connection', (socket) => {
     if (p) p.emit('end-call');
     activeCalls.delete(socket.id);
   });
+}); 
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
