12b8e7f6578abd150712be493ae2eeec827b210c
sanz <systemvip28@gmail.com>
2026-09-26 12:11:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -280,6 +280,8 @@ io.on('connection', (socket) => {
             job: data.job ? String(data.job).trim() : '-',
             server: data.server,
             profilePic: data.profilePic || '',
+            lat: data.lat || null, 
+            lon: data.lon || null,
             history: new Set(), 
             stories: [], 
             online: true,
@@ -293,6 +295,8 @@ io.on('connection', (socket) => {
         user.job = data.job ? String(data.job).trim() : '-',
         user.server = data.server;
         if(data.profilePic) user.profilePic = data.profilePic;
+        if(data.lat) user.lat = data.lat;
+        if(data.lon) user.lon = data.lon;
         user.online = true;
     }
     user.socketId = socket.id;
@@ -330,6 +334,8 @@ io.on('connection', (socket) => {
               if (data.age) user.age = data.age;
               if (data.gender) user.gender = data.gender;
               if (data.job) user.job = data.job;
+              if (data.lat) user.lat = data.lat;
+              if (data.lon) user.lon = data.lon;
 
               const partnerSocket = getPartnerSocket(userId);
               if (partnerSocket) {
